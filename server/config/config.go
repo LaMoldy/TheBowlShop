71e6fe9a33f1da0fb2a64f1b2e903a8ce5d8077d
Nikk Jackson <NikkolasJ@Outlook.com>
2024-04-20 01:08:13 -0400

    feat: initial commit.  Added config functionality for api.

---
+++ server/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+type ServerConfig struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+}
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Name     string `yaml:"name"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+}
+
+// Loads the config file
+func LoadConfig() (*Config, error) {
+	// Get the data from the file
+	data, err := os.ReadFile("config/config.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	// Place data into config
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
